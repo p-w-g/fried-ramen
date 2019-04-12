41aae3f731699cf8643899eec3cb0c22135fed83
Przemek Grzelczyk <przemek.grzelczyk@appliedtechnology.se>
2019-04-12 09:12:21 +0200

    migration between machines

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,15 @@
 <template>
   <div id="app">
-    <img
-      alt="Vue logo"
-      src="./assets/logo.png"
-    >
-    <HelloWorld msg="Welcome to Your Vue.js App" />
     <List ramen="nom nom" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 import List from './components/List.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
     List,
   },
 };
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,18 +1,39 @@
 <template>
   <div>
-    <h1>List component</h1>
+    <h1>List of food I'd wanna eat this week</h1>
     <p>{{ ramen }}</p>
-    <form 
-      v-on:submit.prevent
-      action="#"
+    <form
       id="the-form"
+      action="#"
+      @submit.prevent="addNewTodoTask"
     >
-    <input 
-      type="text"
-      v-model="title" 
-      placeholder="Just anything">
-    <button name="the-form"></button>
+      <input
+        id="title"
+        v-model="title"
+        type="text"
+        placeholder="Yakiniku"
+      >
+      <input
+        id="occurence"
+        v-model="occurence"
+        type="text"
+        placeholder="sunday perhaps?"
+      >
+      <button name="the-form">
+        SubMe
+      </button>
     </form>
+    <div>
+      <p
+        v-for="obj in todos"
+        :id="'todo-' + obj.id"
+        :key="obj.id"
+        class="task-card"
+        @click="makeMeDone(obj.id)"
+      >
+       boring print: title: {{ obj.title }}, occurence: {{ obj.occurence }}, stateDone: {{obj.isDone}}, id: {{obj.id}}
+      </p>
+    </div>
   </div>
 </template>
 
@@ -21,13 +42,45 @@ export default {
   name: 'List',
   props: {
     ramen: String,
-    title: String,
   },
-  
-methods: {
-  debug (event) {
-    console.log(event.target.name)
-  }
-} 
+  data: () => ({
+    todos: [],
+    title: '',
+    occurence: '',
+    id: 0,
+    isDone: false,
+  }),
+  methods: {
+    addNewTodoTask() {
+      this.todos.push({
+        title: this.title,
+        occurence: this.occurence,
+        id: this.id++,
+        isDone: false,
+      });
+
+      this.resetForm();
+    },
+    resetForm() {
+      this.title = '';
+      this.occurence = '';
+    },
+    // makeMeDone(event){
+    //   this.isDone = true;
+    //   console.log("Am I dun?: ", this.isDone, "from: ", event.target.id);
+      
+    // }
+    makeMeDone(passMeAnID){
+      this.isDone = true;
+      console.log("Am I dun?: ", this.isDone, "from: ", passMeAnID);
+      
+    }
+  },
 };
 </script>
+<style>
+
+.done{
+  background-color: #89b2db;
+}
+</style>
