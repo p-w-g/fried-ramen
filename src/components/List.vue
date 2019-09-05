46803889a31a00cf20477dcd97715fb28b99cc79
przemek <p.grzelczyk@gmail.com>
2019-09-05 19:28:52 +0200

    updated naming convention

--- src/components/List.vue
+++ src/components/List.vue
@@ -4,43 +4,43 @@
     <form
       id="expenses-form"
       action="#"
-      @submit.prevent="addNewTodoTask"
+      @submit.prevent="addNewExpense"
     >
       <input
-        id="task"
-        v-model="task"
+        id="expense"
+        v-model="expense"
         type="text"
-        placeholder="For?"
+        placeholder="Expense?"
       >
       <input
-        id="value"
-        v-model="value"
+        id="amount"
+        v-model="amount"
         type="number"
-        placeholder="How much?"
+        placeholder="Amount?"
       >
       <button name="expenses-form">
         Add
       </button>
     </form>
     <div>
-      <p>Total: <b>{{ totalCost }}</b></p>
-      <p>Active: <b>{{ activeCosts }}</b></p>
-      <p>Frozen: <b>{{ frozenCosts }}</b></p>
+      <p>Total: <b>{{ totalExpenses }}</b></p>
+      <p>Active: <b>{{ activeExpensesTotal }}</b></p>
+      <p>Frozen: <b>{{ postponedExpensesTotal }}</b></p>
 
       <article>
         <p
-          v-for="obj in activeTasks"
+          v-for="obj in activeExpenses"
           :key="obj.id"
-          :task="obj.task"
-          class="task-card"
+          :expense="obj.expense"
+          class="expense-card"
         >
-          <b>{{ obj.task }}:</b>
-          {{ obj.value }}
+          <b>{{ obj.expense }}:</b>
+          {{ obj.amount }}
           <button @click="remove(obj.id)">
-            Paid off
+            Expedite
           </button>
           <button @click="freeze(obj.id)">
-            Freeze it
+            Postpone
           </button>
         </p>
       </article>
@@ -48,18 +48,18 @@
     <hr>
     <div>
       <article>
-        Frozen / awaiting
+        Postponed
         <p
-          v-for="obj in frozenTasks"
+          v-for="obj in postponedExpenses"
           :key="obj.id"
-          :task="obj.task"
-          class="task-card"
+          :expense="obj.expense"
+          class="expense-card"
         >
-          <b>{{ obj.task }}:</b>
-          {{ obj.value }}
+          <b>{{ obj.expense }}:</b>
+          {{ obj.amount }}
 
-          <button @click="defrost(obj.id)">
-            defrost
+          <button @click="advance(obj.id)">
+            Advance
           </button>
         </p>
       </article>
@@ -72,28 +72,27 @@ export default {
   name: 'List',
   data: () => ({
     todos: [],
-    task: '',
-    value: '',
-    totalMinus: 0,
-    isCurrentTaskFrozen: false,
+    expense: '',
+    amount: '',
+    isCurrentExpensePostponed: false,
     id: 0,
   }),
 
   computed: {
-    totalCost() {
+    totalExpenses() {
       return this.reducer(this.todos);
     },
-    frozenCosts() {
-      return this.reducer(this.frozenTasks);
+    postponedExpensesTotal() {
+      return this.reducer(this.postponedExpenses);
     },
-    frozenTasks() {
-      return this.todos.filter(u => u.isCurrentTaskFrozen);
+    postponedExpenses() {
+      return this.todos.filter(u => u.isCurrentExpensePostponed);
     },
-    activeCosts() {
-      return this.reducer(this.activeTasks);
+    activeExpensesTotal() {
+      return this.reducer(this.activeExpenses);
     },
-    activeTasks() {
-      return this.todos.filter(u => !u.isCurrentTaskFrozen);
+    activeExpenses() {
+      return this.todos.filter(u => !u.isCurrentExpensePostponed);
     },
   },
 
@@ -104,23 +103,23 @@ export default {
   },
 
   methods: {
-    addNewTodoTask() {
-      return (this.value !== '') ? (this.newObjectPush(), this.resetForm(), this.saveTodos()) : null;
+    addNewExpense() {
+      return (this.amount !== '') ? (this.newObjectPush(), this.resetForm(), this.saveTodos()) : null;
     },
     newObjectPush() {
       this.todos.push({
-        task: this.task,
-        value: this.value,
-        isCurrentTaskFrozen: false,
-        id: this.id++,
+        expense: this.expense,
+        amount: this.amount,
+        isCurrentExpensePostponed: false,
+        id: this.id += 1,
       });
     },
     freeze(index) {
-      this.todos[index].isCurrentTaskFrozen = true;
+      this.todos[index].isCurrentExpensePostponed = true;
       this.saveTodos();
     },
-    defrost(index) {
-      this.todos[index].isCurrentTaskFrozen = false;
+    advance(index) {
+      this.todos[index].isCurrentExpensePostponed = false;
       this.saveTodos();
     },
     parser10(notAStrinObviously) {
@@ -128,12 +127,12 @@ export default {
     },
     reducer(arrayOfObjects) {
       return arrayOfObjects.reduce(
-        (accumulator, currentObject) => accumulator + this.parser10(currentObject.value), 0,
+        (accumulator, currentObject) => accumulator + this.parser10(currentObject.amount), 0,
       );
     },
     resetForm() {
-      this.task = '';
-      this.value = '';
+      this.expense = '';
+      this.amount = '';
     },
     remove(index) {
       this.$delete(this.todos[index]);
@@ -155,7 +154,5 @@ export default {
 };
 </script>
 <style>
-.done {
-  background-color: #89b2db;
-}
+
 </style>
