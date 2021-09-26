a5fb76363faeb96ac0319f7b5d4946600ab6f7b2
p-w-g <p.grzelczyk@gmail.com>
2021-09-26 09:45:22 +0200

    vue add typescript

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   verbose: true,
   coverageReporters: ['text', 'html'],
   collectCoverage: true,
