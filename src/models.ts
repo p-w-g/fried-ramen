ea0529f50ed0a3732dc5f13fbd65560aba6a48d8
p-w-g <p.grzelczyk@gmail.com>
2022-10-21 11:44:48 +0200

    update model with description

--- src/models.ts
+++ src/models.ts
@@ -1,6 +1,7 @@
 export class expenseModel {
   Expense: string;
   Amount: number;
+  Description?: string;
   Id: number;
   Label?: string;
   isPostponed: boolean;
@@ -8,6 +9,7 @@ export class expenseModel {
   constructor() {
     this.Expense = '';
     this.Amount = 0;
+    this.Description = '';
     this.Id = 0;
     this.Label = '';
     this.isPostponed = false;
