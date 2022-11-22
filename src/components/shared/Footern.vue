bfdc15f89d7e9cb5e999a0d8b7aa5705af7b67ae
p-w-g <p.grzelczyk@gmail.com>
2022-11-22 14:35:15 +0100

    bootstrap multilist footer

---
+++ src/components/shared/Footern.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="fr-footer">
+    <button disabled="disabled">1</button>
+    <button disabled="disabled">2</button>
+    <button disabled="disabled">3</button>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'fr-footer',
+};
+</script>
+
+<style lang="scss">
+.fr-footer {
+}
+</style>
