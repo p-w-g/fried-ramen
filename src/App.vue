8039d853906ea15db7959cdaf8f19f5a5ce3ea20
p-w-g <p.grzelczyk@gmail.com>
2022-11-22 16:51:55 +0100

    import footer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <expense-list />
+    <footern />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import ExpenseList from './views/List.vue';
+import Footern from './components/shared/Footern.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     ExpenseList,
+    Footern,
   },
 });
 </script>
