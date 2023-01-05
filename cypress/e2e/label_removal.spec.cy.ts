9aec3a26a40b0cd67da9043744460fcb156008dc
p-w-g <p.grzelczyk@gmail.com>
2023-01-05 16:37:40 +0100

    write case for broken label removal

--- cypress/e2e/label_removal.spec.cy.ts
+++ cypress/e2e/label_removal.spec.cy.ts
@@ -1,5 +1,24 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:8080')
-  })
-})
+describe('Correct label removal', () => {
+  it('adds and removes empty label', () => {
+    cy.visit('http://localhost:8080');
+    cy.get('input.accordion.accordion--close').click();
+    cy.get('input#label.fr__input-box').type('first').type('{enter}');
+    cy.get('input#label.fr__input-box').type('second').type('{enter}');
+    cy.get('input#label.fr__input-box').type('third').type('{enter}');
+
+    cy.get('select#removal-menu').select('first');
+
+    // should be second and third only
+    cy.get(
+      '#app > div > div > div.fr__content-column > div:nth-child(2) > h2'
+    ).should('not.include.text', 'first: 0');
+
+    cy.get(
+      '#app > div > div > div.fr__content-column > div:nth-child(2) > h2'
+    ).should('include.text', 'second: 0');
+
+    cy.get(
+      '#app > div > div > div.fr__content-column > div:nth-child(3) > h2'
+    ).should('include.text', 'third: 0');
+  });
+});
